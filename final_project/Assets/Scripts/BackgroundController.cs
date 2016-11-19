4765f7f8857ad3647c5078f16610af11f345f7f8
Umit Karasu <umitkarasu@yahoo.ca>
2016-11-19 12:28:21 -0500

    Game Development Final Project
    Power Ninja

---
+++ final_project/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+
+
+	//create transform and y postion variables for movement and position
+	//of the background sprites
+	private Transform _transform;
+	float yPos;
+
+
+//	variable to get size of the background sprites
+	SpriteRenderer render;
+
+
+	// Use this for initialization
+	void Start () {
+
+		_transform = gameObject.GetComponent<Transform> ();
+		yPos = _transform.position.y;
+//		// Get the render component for size of the gameobject
+        render = gameObject.GetComponent<SpriteRenderer>();
+
+
+	}
+		
+
+	//when background became invisible
+	void OnBecameInvisible()
+	{
+		//get the background x coordinate
+		float width = render.bounds.size.x;
+		//calculate current position
+		var backgrndPosition = gameObject.transform.position;
+		//calculate new position;
+		float X = backgrndPosition.x + width*2;
+
+		//move the background to new position when invisible
+		gameObject.transform.position =new Vector2(X,yPos);
+	}
+}
---
+++ final_project/Assets/Scripts/NinjaController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NinjaController : MonoBehaviour {
+
+	//speed variable for crazy_bullet
+	[SerializeField]
+	private float speed;
+
+	//a fireball gameobject to connect to the crazy_bullet
+//	public GameObject fireball;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPosition = _transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		_currentPosition = _transform.position;
+
+		//move up
+		if (Input.GetKey (KeyCode.UpArrow))
+			_currentPosition += new Vector2 (0,speed);
+
+		//move down
+		if (Input.GetKey (KeyCode.DownArrow))
+			_currentPosition -= new Vector2 (0,speed);
+
+		//move left
+		if (Input.GetKey (KeyCode.LeftArrow))
+			_currentPosition -= new Vector2 (speed,0);
+
+		//move right
+		if (Input.GetKey (KeyCode.RightArrow))
+			_currentPosition += new Vector2 (speed,0);
+
+
+		//check bounds of screen
+//		checkBounds ();
+		_transform.position = _currentPosition;
+
+//		if (Input.GetKeyDown("space")) {
+//			// Create a new fireball at “transform.position”
+//			// Which is the current position of the crazy_bullet
+//			Instantiate(fireball, transform.position, Quaternion.identity);
+//		}
+
+	}
+//	//check the bounds for Crazy_bullet
+//	private void checkBounds(){
+//		//get the height of the crazy_bullet
+//		var renderer = gameObject.GetComponent<SpriteRenderer>();
+//		float heightCrazyBullet = renderer.bounds.size.y;
+//
+//
+//
+//		//if position is bigger than camera view-half of the height of CB(otherwise the half of CB will be out of screen)
+//		//for the top boundry
+//		if (_currentPosition.y > Camera.main.orthographicSize-heightCrazyBullet/2.0f) {
+//			_currentPosition.y = Camera.main.orthographicSize-heightCrazyBullet/2.0f;
+//		}
+//
+//		//for the bottom boundry (grass height added -->1.4f)
+//		if (_currentPosition.y < -Camera.main.orthographicSize+heightCrazyBullet/2.0f+1.4f) {
+//			_currentPosition.y = -Camera.main.orthographicSize+heightCrazyBullet/2.0f+1.4f;
+//		}
+//
+//		//for the right  boundry
+//		if (_currentPosition.x > 12.9f) {
+//			_currentPosition.x = 12.9f;
+//		}
+//
+//		//for the left boundry
+//		if (_currentPosition.x < -12.9f) {
+//			_currentPosition.x = -12.9f;
+//		}
+//
+//	}
+}
